bf372086be8f44074b853436f6b441d121a437c1
rajjo <rajnsihtiwari96766@gmail.com>
2023-01-17 12:31:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Header from "./Header";
+
+import React from "react";
+
+function App() {
+  // let x=10, y=20;
+  // let modulename="React";
+  // let y=20;
+  return (
+
+    <div>
+      <Header/>
+    <button>Add</button>
+
+    <div>
+      <span>Name</span><brs/>
+      <span>Phone</span>
+      mbkjkkj
+      <span>Rajnish</span>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Header = function () {
+    return (
+        < div className="header" >
+            Phone directory - 2
+        </div >
+    )
+}
+
+export default Header;
